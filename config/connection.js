38b2ca0d283d398978458efdc0e4e5e5c558bf31
Morgan Dorfman <mdorfman21@gmail.com>
2019-04-23 18:55:03 -0400

    works locally

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,21 @@
 let mysql = require("mysql");
 
-var connection;
+const db = process.env.JAWSDB_URL || {
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
+};
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-  });
-}
+let connection = mysql.createConnection(db);
 
-connection.connect(function(err) {
+connection.connect(err => {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.log(`there was an error connecting ${err.stack}`);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log(`connected as ${connection.threadId} on http://localhost:3306`);
 });
 
 module.exports = connection;
