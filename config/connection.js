94fafe492ffea664a0d8901a7043922195e5d223
Morgan Dorfman <mdorfman21@gmail.com>
2019-04-23 18:53:41 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,25 @@
 let mysql = require("mysql");
 
-const db = process.env.JAWSDB_URL || {
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-};
+var connection;
 
-let connection = mysql.createConnection(db);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
-connection.connect(err => {
+connection.connect(function(err) {
   if (err) {
-    console.log(`there was an error connecting ${err.stack}`);
+    console.error("error connecting: " + err.stack);
     return;
   }
-  console.log(`connected as ${connection.threadId} on http://localhost:3306`);
+  console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
